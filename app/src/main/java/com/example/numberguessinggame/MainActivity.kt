328048bda405ae11d6d5570a95eebb86c4ea73f0
Bhavye2003Developer <bhavyedevelopment2003@gmail.com>
2023-01-13 11:58:31 +0530

    first update

--- app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.numberguessinggame
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -18,14 +19,21 @@ import com.example.numberguessinggame.R.id.textView
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
 
+    lateinit var background : ConstraintLayout
+    lateinit var textView: TextView
+    lateinit var btnLeft : Button
+    lateinit var btnRight : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnLeft : Button = findViewById(R.id.btnLeft)
-        val btnRight : Button = findViewById(R.id.btnRight)
-        val textView : TextView = findViewById(R.id.textView)
-        val background : ConstraintLayout = findViewById<ConstraintLayout>(R.id.background)
+        btnLeft = findViewById(R.id.btnLeft)
+        btnRight = findViewById(R.id.btnRight)
+        textView = findViewById(R.id.textView)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            background = findViewById<ConstraintLayout>(R.id.background)
+        }
         var num_correct : Int = 0
         var num_wrong : Int = 0
         var text = ""
@@ -48,21 +56,11 @@ class MainActivity : AppCompatActivity() {
                     x = 1
                     y = 2
                 }
-                btnLeft.setText(x.toString())
-                btnRight.setText(y.toString())
-                Toast.makeText(this,"Right answer",Toast.LENGTH_SHORT).show()
-                background.setBackgroundColor(getColor(R.color.green))
-                text = "Number of right answers : $num_correct\nNumber of wrong answers : $num_wrong"
-                textView.setText(text)
-                textView.setTextColor(getColor(R.color.black))
+                modify_screen(x,y,true,R.color.green,num_correct,num_wrong,R.color.black)
             }
             else{
                 num_wrong++
-                Toast.makeText(this,"Wrong answer",Toast.LENGTH_SHORT).show()
-                background.setBackgroundColor(getColor(R.color.red))
-                text = "Number of right answers : $num_correct\nNumber of wrong answers : $num_wrong"
-                textView.setText(text)
-                textView.setTextColor(getColor(R.color.white))
+                modify_screen(x,y,false,R.color.red,num_correct,num_wrong,R.color.white)
             }
         }
         btnRight.setOnClickListener {
@@ -74,22 +72,29 @@ class MainActivity : AppCompatActivity() {
                     x = 1
                     y = 2
                 }
-                btnLeft.setText(x.toString())
-                btnRight.setText(y.toString())
-                Toast.makeText(this,"Right answer",Toast.LENGTH_SHORT).show()
-                background.setBackgroundColor(getColor(R.color.green))
-                text = "Number of right answers : $num_correct\nNumber of wrong answers : $num_wrong"
-                textView.setText(text)
-                textView.setTextColor(getColor(R.color.black))
+                modify_screen(x,y,true,R.color.green,num_correct,num_wrong,R.color.black)
             }
             else{
                 num_wrong++
-                Toast.makeText(this,"Wrong answer",Toast.LENGTH_SHORT).show()
-                background.setBackgroundColor(getColor(R.color.red))
-                text = "Number of right answers : $num_correct\nNumber of wrong answers : $num_wrong"
-                textView.setText(text)
-                textView.setTextColor(getColor(R.color.white))
+                modify_screen(x,y,false,R.color.red,num_correct,num_wrong,R.color.white)
             }
+        }
+    }
+
+    @SuppressLint("NewApi")
+    fun modify_screen(x : Int, y : Int, right_wrong : Boolean, background_color : Int, num_correct : Int, num_wrong: Int, text_color : Int){
+        val text = "Number of right answers : $num_correct\nNumber of wrong answers : $num_wrong"
+        background.setBackgroundColor(getColor(background_color))
+        textView.setText(text)
+        if (right_wrong){
+            btnLeft.setText(x.toString())
+            btnRight.setText(y.toString())
+            Toast.makeText(this,"Right answer",Toast.LENGTH_SHORT).show()
+            textView.setTextColor(getColor(text_color))
+        }
+        else{
+            Toast.makeText(this,"Wrong answer",Toast.LENGTH_SHORT).show()
+            textView.setTextColor(getColor(text_color))
         }
     }
 }
